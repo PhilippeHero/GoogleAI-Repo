1851adb1558f933b5ce73dec1f96d371e8f93ec0
PhilippeHero <philippe.herau@impalium.net>
2025-09-09 17:25:15 +0200

    feat: Enhance SEO and Supabase auth
    Updates the `index.html` to include meta tags for SEO and social sharing, improving discoverability and presentation.
    Configures Supabase client in `src/supabase.ts` to enable local storage for session persistence, auto-refresh, and URL detection, ensuring a more robust authentication flow.

--- src/supabase.ts
+++ src/supabase.ts
@@ -9,6 +9,12 @@ const supabaseUrl = 'https://vfkdunbfkrvbwdueejcj.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZma2R1bmJma3J2YndkdWVlamNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDQyNTQsImV4cCI6MjA3MjkyMDI1NH0.vhm-IUPErmFof0695Seff0RcgLzuhjNj27rpn-EczsQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: window.localStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
   global: {
     // This header helps to ensure the client always fetches the latest schema
     // from the server, preventing errors caused by a stale schema cache.
